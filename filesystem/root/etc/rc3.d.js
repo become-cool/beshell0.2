48419b79780790269ee8c267fc62cb20d5dc5811
alee <alee@chou.im>
2022-01-16 23:16:16 +0800

    works

---
+++ filesystem/root/etc/rc3.d.js
@@ -0,0 +1,37 @@
+(async function(){
+    try{
+        require("/lib/base/utils.js")
+        
+        require("./init.d/repl.js")
+
+        require("./init.d/banner.js")()
+
+        if(!process.simulate) {
+            global.WiFi = require("besdk/wifi")
+        }
+
+        await require("./init.d/display.js")()
+        
+        require("lv")
+        await sleep(0)
+        
+        console.log('BeShell is ready.')
+
+        if(beapi.reset_app) {
+            let pkgpath = beapi.reset_app + "/package.json"
+            if( !beapi.fs.existsSync(pkgpath) ) {
+                console.log("path is not a app folder", beapi.reset_app)
+            }
+            else {
+                let pkg = JSON.load(pkgpath)
+                let mainpath = beapi.reset_app + '/' + (pkg.main || "index.js")
+                require(mainpath)
+            }
+        }
+
+    }
+    catch(e) {
+        console.log(e.message)
+        console.log(e.stack)
+    }
+})()

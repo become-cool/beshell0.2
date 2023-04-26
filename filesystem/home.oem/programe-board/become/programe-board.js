7672ca4e3145d40ca38d25b6205ddcb4ba6b33f3
alee <alee@chou.im>
2023-04-26 20:04:24 +0800

    remoev oem files

--- filesystem/home.oem/programe-board/become/programe-board.js
+++
@@ -1,9 +0,0 @@
-const miso = pin(12).setMode("input")
-const pl = pin(13).setMode("output")
-const sck = pin(14).setMode("output")
-const cs = pin(15).setMode("output")
-
-global.read = function() {
-        
-}
-
